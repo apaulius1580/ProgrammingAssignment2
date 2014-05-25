1bc4d15ec75af35586b60a828b6c59144cbfe187
Andrius Paulius <andrius.paulius@gmail.com>
2014-05-25 14:40:11 -0700

    Matrix Cache Code Version 1.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Function returns 4 function that allow you to get/set the matrix/inverse
+## the matrix and inverse are stored in state variables that persist outside the function scope.
 
+
+makeCacheMatrix <- function(m = matrix()) {
+    inv<-NULL
+    set <- function(y) {
+        m <<- y
+        inv <<- NULL
+    }
+    get <- function() m
+    setInverse <- function(inverse) inv <<- inverse
+    getInverse <- function() inv
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Check if inverse exists in cached variable inv, if so return it
+## otherwise calc it using the solve function.. 
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(m, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- m$getInverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- m$get()
+    inv <- solve(data)
+    m$setmean(inv)
+    inv
+
 }
+
